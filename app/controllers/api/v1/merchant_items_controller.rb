dc8f58367f6c82997321d0528056fca4a1d3d9e7
Emmy Morris <emmymorris1102@gmail.com>
2021-07-21 16:55:31 -0600

    Add merchant items

---
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::MerchantItemsController < ApplicationController
+  def index
+    m = Merchant.find(params[:merchant_id])
+    items = m.items
+    render json: ItemSerializer.new(items).serialized_json
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,6 @@ class ApplicationController < ActionController::API
       render_api_error(e.message, 404)
     rescue ActiveRecord::RecordInvalid => e
       render_api_error(e.record.errors.full_messages, 422)
-    rescue JWT::ExpiredSignature => e
-      render_api_error(e.message, 401)
-    rescue InvalidTokenError => e
-      render_api_error(e.message, 422)
-    rescue MissingTokenError => e
-      render_api_error(e.message, 422)
     end
 
     def render_api_error(messages, code)
