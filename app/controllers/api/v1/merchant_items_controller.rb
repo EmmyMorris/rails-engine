f38763f8dd6789a327dbf8703c260cf3ff6aa582
Emmy Morris <emmymorris1102@gmail.com>
2021-07-22 09:26:42 -0600

    take out pry

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::MerchantItemsController < ApplicationController
   def index
     m = Merchant.find(params[:merchant_id])
     items = m.items
+    # require "pry"; binding.pry
     render json: ItemSerializer.new(items).serialized_json
   end
 end
--- spec/requests/api/v1/merchants_items_request_spec.rb
+++ spec/requests/api/v1/merchants_items_request_spec.rb
@@ -6,6 +6,7 @@ describe "A Merchants Items" do
     @merchant = create(:merchant)
     @item1 = create(:item, merchant: @merchant)
     @item2 = create(:item, merchant: @merchant)
+    @item3 = create(:item, merchant: @merchant)
     get api_v1_merchant_items_path(merchant_id: @merchant.id)
     @parsed_merchants = JSON.parse(response.body, symbolize_names: true)
     @merchants = @parsed_merchants[:data]
@@ -16,10 +17,12 @@ describe "A Merchants Items" do
     expect(response).to be_successful
 
     expect(@merchants).not_to be_empty
-    expect(@merchants.count).to eq(2)
+    expect(@merchants.count).to eq(3)
     expect(@merchants[0].count).to eq(3)
+    # require "pry"; binding.pry
     expect(@merchants[0][:id].to_i).to eq(@item1.id)
     expect(@merchants[1][:id].to_i).to eq(@item2.id)
+    expect(@merchants[2][:id].to_i).to eq(@item3.id)
   end
 
   it "sad path, bad integer id returns 404" do
