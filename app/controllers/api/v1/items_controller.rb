ea89cfaf3f76cba459b3be5d3913af2d298039a8
Emmy Morris <emmymorris1102@gmail.com>
2021-07-22 15:31:32 -0600

    Search for one merchant

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     i = Item.offset((page_number - 1) * per_page).limit(per_page)
-    render json: ItemSerializer.new(i).serialized_json
+    render json: ItemSerializer.new(i).serialized_json, status: :ok
   end
 
   def show
     i = Item.find(params[:id])
-    render json: ItemSerializer.new(i).serialized_json
+    render json: ItemSerializer.new(i).serialized_json, status: :ok
   end
 end
--- spec/requests/api/v1/merchants_items_request_spec.rb
+++ spec/requests/api/v1/merchants_items_request_spec.rb
@@ -8,21 +8,20 @@ describe "A Merchants Items" do
     @item2 = create(:item, merchant: @merchant)
     @item3 = create(:item, merchant: @merchant)
     get api_v1_merchant_items_path(merchant_id: @merchant.id)
-    @parsed_merchants = JSON.parse(response.body, symbolize_names: true)
-    @merchants = @parsed_merchants[:data]
+    @parsed_merchant_items = JSON.parse(response.body, symbolize_names: true)
+    @merchant_items = @parsed_merchant_items[:data]
   end
 
   it "happy path, fetch all items" do
     expect(response).to have_http_status(200)
     expect(response).to be_successful
 
-    expect(@merchants).not_to be_empty
-    expect(@merchants.count).to eq(3)
-    expect(@merchants[0].count).to eq(3)
-    # require "pry"; binding.pry
-    expect(@merchants[0][:id].to_i).to eq(@item1.id)
-    expect(@merchants[1][:id].to_i).to eq(@item2.id)
-    expect(@merchants[2][:id].to_i).to eq(@item3.id)
+    expect(@merchant_items).not_to be_empty
+    expect(@merchant_items.count).to eq(3)
+    expect(@merchant_items[0].count).to eq(3)
+    expect(@merchant_items[0][:id].to_i).to eq(@item1.id)
+    expect(@merchant_items[1][:id].to_i).to eq(@item2.id)
+    expect(@merchant_items[2][:id].to_i).to eq(@item3.id)
   end
 
   it "sad path, bad integer id returns 404" do
