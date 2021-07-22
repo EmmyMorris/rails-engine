ed21f28812a8b05ff2f4a1104436edded24b7a86
Emmy Morris <emmymorris1102@gmail.com>
2021-07-22 17:53:38 -0600

    Start merchants with most revenue

--- app/controllers/api/v1/revenues_controller.rb
+++ app/controllers/api/v1/revenues_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::RevenuesController < ApplicationController
   def top_merchants
-    require "pry"; binding.pry
+    m = Merchant.most_revenue(params[:quantity])
+    render json: MerchantSerializer.new(m).serialized_json, status: :ok
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,14 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.most_revenue(quantity)
+    joins(invoices: [:invoice_items, :transactions])
+    .where('transactions.result = ?', 'success')
+    .where('invoices.status = ?', 'shipped')
+    .group(:id)
+    .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+    .order('revenue DESC')
+    .limit(10)
+  end
 end
